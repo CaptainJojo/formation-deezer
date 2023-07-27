595281b3ec880eb491196f07a5d2b42362959ce6
Jonathan Jalouzot <jonathan.jalouzot@gmail.com>
2023-07-27 12:27:54 +0200

    feat: type

--- src/resolvers/artist/fans.ts
+++ src/resolvers/artist/fans.ts
@@ -1,5 +1,5 @@
-export const fans = async ({ id }, _, { datasources }) => {
-  const fans = await datasources.fan.getFansByArtist({ artistId: id });
+export const fans = async (parent, _, { datasources }) => {
+  const fans = await datasources.fan.getFansByArtist({ artistId: parent.id });
 
   return fans.fans;
 };
