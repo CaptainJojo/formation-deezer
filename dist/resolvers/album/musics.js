35a4dac96cda661d5240cc6dc016db5553e2f978
Jonathan Jalouzot <jonathan.jalouzot@gmail.com>
2023-07-28 14:15:34 +0200

    feat: add schremas

--- dist/resolvers/album/musics.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.musics = void 0;
-const musics = async ({ id }, __, { datasources }) => {
-    const musics = await datasources.music.getMusicsByAlbum({ albumId: id });
-    return musics;
-};
-exports.musics = musics;
--- dist/resolvers/artist/fans.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fans = void 0;
-const fans = async (parent, _, { datasources }) => {
-    const fans = await datasources.fan.getFansByArtist({ artistId: parent.id });
-    return fans.fans;
-};
-exports.fans = fans;
